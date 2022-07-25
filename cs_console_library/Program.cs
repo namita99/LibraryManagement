426e01434015cf68791a2d6040c481f2f77da30f
Namita <namita@simplilearn.com>
2022-07-25 11:41:47 +0530

    Add project files.

---
+++ cs_console_library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_console_library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
